d4e090fa2a5f929e0ffa4fa20b97dd84b9ef2499
Markus Warne <markus.warne@gmail.com>
2018-05-28 23:37:17 +0200

    Start with main functionality

---
+++ src/handlers/suggestionsHandler.js
@@ -0,0 +1,3 @@
+exports.addSuggestionHandler = (req, res) => res.send('Added suggestion');
+exports.visitedSuggestionHandler = (req, res) => res.send('Visited suggestion');
+exports.getSuggestionsHandler = (req, res) => res.send('Here are some suggestions');
--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const { placesSearchByIdHandler, placesSearchHandler } = require('./handlers/placesHandler.js');
+const {
+  placesSearchByIdHandler,
+  placesSearchHandler
+} = require('./handlers/placesHandler.js');
 const { distanceByIdHandler } = require('./handlers/distanceHandler.js');
+const {
+  addSuggestionHandler,
+  visitedSuggestionHandler,
+  getSuggestionsHandler
+} = require('./handlers/suggestionsHandler.js');
 
 // read env variables from .env
 require('dotenv').config();
@@ -9,9 +17,16 @@ const PORT = process.env.PORT || 1337;
 const app = express();
 
 // route handlers
-app.get('/', (req, res) => res.send('Well, hello to you good sir!'));
+app.get('/', (req, res) => res.send('Hello to you good sir!'));
+
+// utility endpoints
 app.get('/search/:query', placesSearchHandler);
 app.get('/search/id/:placeId', placesSearchByIdHandler);
 app.get('/distance/:placeId', distanceByIdHandler);
+
+// main
+app.post('/suggestion', addSuggestionHandler);
+app.put('/suggestion/:placeId', visitedSuggestionHandler);
+app.get('/suggestion', getSuggestionsHandler);
 
 app.listen(PORT, () => console.log(`Lunchbot service running on port ${PORT}`));
