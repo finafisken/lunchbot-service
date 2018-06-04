0c419ab38ca1ac0465fe294e2d9ced26e170b9c4
Markus Warne <markus.warne@gmail.com>
2018-06-04 20:31:30 +0200

    CORS support for clients

--- src/server.js
+++ src/server.js
@@ -16,17 +16,33 @@ require('dotenv').config();
 const PORT = process.env.PORT || 1337;
 const app = express();
 
+// allow CORS request from clients
+app.use(function(req, res, next) {
+  const allowedHeaders = [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Authentication'
+  ].join(',');
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', allowedHeaders);
+  next();
+});
+
 // route handlers
 app.get('/', (req, res) => res.send('Hello to you good sir!'));
-
-// utility endpoints
-app.get('/search/:query', placesSearchHandler);
-app.get('/search/id/:placeId', placesSearchByIdHandler);
-app.get('/distance/:placeId', distanceByIdHandler);
 
 // main
 app.post('/suggestion/:placeId', addSuggestionHandler);
 app.put('/suggestion/:placeId', visitedSuggestionHandler);
 app.get('/suggestion', getSuggestionsHandler);
+
+// utility endpoints
+app.get('/search/:query', placesSearchHandler);
+app.get('/search/id/:placeId', placesSearchByIdHandler);
+app.get('/distance/:placeId', distanceByIdHandler);
 
 app.listen(PORT, () => console.log(`Lunchbot service running on port ${PORT}`));
