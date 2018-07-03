b9924a193a1d3de64b84ca07402b5099b0e666b0
Markus Warne <markus.warne@viaplay.com>
2018-07-03 17:34:15 +0200

    Sort suggestions list by lastvisited

--- app/core/listSuggestions.js
+++ app/core/listSuggestions.js
@@ -1,5 +1,13 @@
 const dynamodb = require('../services/dynamodb.js');
 
-const listSuggestions = () => dynamodb.listPlaces();
+const sortByLastvisited = (a, b) => {
+  if (a.lastVisitedAt === b.lastVisitedAt) return 0;
+  return a.lastVisitedAt < b.lastVisitedAt ? -1 : 1;
+};
+
+const listSuggestions = async () => {
+  const places = await dynamodb.listPlaces();
+  return places.sort(sortByLastvisited);
+}
 
 module.exports = listSuggestions;
