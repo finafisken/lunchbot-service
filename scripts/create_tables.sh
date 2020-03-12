dbd9453f2e857f8ba9a85cfd2e6c6a6cacf0a8f2
Markus Warne <markus.warne@nentgroup.com>
2020-03-12 17:35:52 +0100

    create table in local dynamodb

--- scripts/create_tables.sh
+++ scripts/create_tables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -m
 
 java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb \
-& aws dynamodb create-table --table-name lunchbot-location-db --attribute-definitions AttributeName=orderId,AttributeType=S --key-schema AttributeName=orderId,KeyType=HASH --billing-mode PAY_PER_REQUEST --endpoint-url http://localhost:8000
-
+& aws dynamodb create-table --table-name lunchbot-location-db --attribute-definitions AttributeName=orderId,AttributeType=S --key-schema AttributeName=orderId,KeyType=HASH --billing-mode PAY_PER_REQUEST --endpoint-url http://localhost:8000 
+fg
