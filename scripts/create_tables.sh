d7afc58f566e90bb497d50fdd6231148065aabb6
Markus Warne <markus.warne@nentgroup.com>
2020-03-13 13:44:56 +0100

    populate local db with mock data

--- scripts/create_tables.sh
+++ scripts/create_tables.sh
@@ -2,5 +2,6 @@
 set -m
 
 java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb \
-& aws dynamodb create-table --table-name lunchbot-location-db --attribute-definitions AttributeName=orderId,AttributeType=S --key-schema AttributeName=orderId,KeyType=HASH --billing-mode PAY_PER_REQUEST --endpoint-url http://localhost:8000 
+& aws dynamodb create-table --table-name lunchbot-location-db --attribute-definitions AttributeName=placeId,AttributeType=S --key-schema AttributeName=placeId,KeyType=HASH --billing-mode PAY_PER_REQUEST --endpoint-url http://localhost:8000 \
+&& aws dynamodb batch-write-item --request-items file://locations_mock.json --endpoint-url http://localhost:8000
 fg
